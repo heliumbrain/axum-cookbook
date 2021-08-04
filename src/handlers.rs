675f4311d0dfd0f17ba892222d8c0c05be6aa6b3
heliumbrain <nils@kanevad.se>
2021-08-04 13:54:03 +0200

    Added handlers for Recipes CRUD

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,22 +1,31 @@
 use axum::response;
-use hyper::server::conn::Http;
+
+use crate::models;
 
 pub async fn handler() -> response::Html<&'static str> {
-    response::Html("<h1>Hello, friend!</h1>")
+  response::Html("<h1>Hello, friend!</h1>")
 }
 
-pub async fn get_recipes() -> response::Json<models::RecipesOut> {
-    todo!()
+pub async fn get_recipes() -> response::Json<Vec<models::RecipesOut>> {
+  todo!()
 }
 
-pub async fn get_recipe() -> response::Json<models::RecipeOut> {
-    todo!()
+pub async fn get_recipe(recipe: uuid::Uuid) -> response::Json<models::RecipeOut> {
+  todo!()
 }
 
-pub async fn create_recipe(recipe: models::RecipeIn) -> response::Json<models::RecipeOut> {
-    todo!()
+pub async fn delete_recipe(recipe: uuid::Uuid) -> response::Json<String> {
+  todo!()
 }
 
-pub async fn update_recipe(recipe: models::Recipe) -> response::Json<models::RecipeOut> {
-    todo!()
+pub async fn create_recipe(
+  recipe: models::RecipeIn,
+) -> response::Json<models::RecipeOut> {
+  todo!()
+}
+
+pub async fn update_recipe(
+  recipe: models::Recipe,
+) -> response::Json<models::RecipeOut> {
+  todo!()
 }
--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,19 @@ mod models;
 
 #[tokio::main]
 async fn main() {
-    // build our application with a route
-    let app = route(
-        "/recipes",
-        get(handlers::get_recipes)
-            .post(handlers::create_recipe)
-            .delete(handlers::delete_recipe)
-            .patch(handlers::update_recipe),
-    );
+  // build our application with a route
+  let app = route(
+    "/recipes",
+    get(handlers::get_recipes)
+      .post(handlers::create_recipe)
+      .delete(handlers::delete_recipe)
+      .patch(handlers::update_recipe),
+  );
 
-    // run it
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    hyper::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+  // run it
+  let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+  hyper::Server::bind(&addr)
+    .serve(app.into_make_service())
+    .await
+    .unwrap();
 }
